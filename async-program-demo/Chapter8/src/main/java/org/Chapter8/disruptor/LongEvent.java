3b45dffaa4917fa8b45f07d42e00b24b4cbbc422
moqi <moqimoqidea@gmail.com>
2021-09-11 20:06:41 +0800

    Update Chapter8 disruptor

--- async-program-demo/Chapter8/src/main/java/org/Chapter8/disruptor/LongEvent.java
+++ async-program-demo/Chapter8/src/main/java/org/Chapter8/disruptor/LongEvent.java
@@ -1,13 +1,15 @@
 package org.Chapter8.disruptor;
 
 public class LongEvent {
-	private long value;
 
-	public void set(long value) {
-		this.value = value;
-	}
+    private long value;
 
-	public long get() {
-		return value;
-	}
+    public void set(long value) {
+        this.value = value;
+    }
+
+    public long get() {
+        return value;
+    }
+
 }
--- async-program-demo/Chapter8/src/main/java/org/Chapter8/disruptor/LongEventFactory.java
+++ async-program-demo/Chapter8/src/main/java/org/Chapter8/disruptor/LongEventFactory.java
@@ -3,7 +3,9 @@ package org.Chapter8.disruptor;
 import com.lmax.disruptor.EventFactory;
 
 public class LongEventFactory implements EventFactory<LongEvent> {
-	public LongEvent newInstance() {
-		return new LongEvent();
-	}
+
+    public LongEvent newInstance() {
+        return new LongEvent();
+    }
+
 }
